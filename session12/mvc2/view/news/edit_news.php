fdc9356a79a97e594d10d8212f3cfaf22130de3d
hagiangg <hagiangnguyenphan@gmail.com>
2019-07-13 18:08:36 +0700

    session12

---
+++ session12/mvc2/view/news/edit_news.php
@@ -0,0 +1,10 @@
+<h1>Edit news page</h1>
+<form action="index.php?action=edit_news&id=<?php echo $id?>" method="post">
+	<p>News title
+		<input type="text" name="title" value="<?php echo $editNews['title']?>">
+	</p>
+	<p>Description
+		<textarea name="description" rows="8"><?php echo $editNews['description']?></textarea>
+	</p>
+	<p><input type="submit" name="edit_news_form" value="Edit news"></p>
+</form>
